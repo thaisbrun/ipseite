25f8d3bd82a12d03ab76bdc51ecbb07d6ba10718
Thaïs Brun <travailthais@outlook.fr>
2023-11-05 18:52:29 +0100

    maj 05/11

--- ipseite/views.py
+++ ipseite/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from ipseite.models import Artist
 # Create your views here.
 def index(request):
-    return render(request, 'home/index.html')
+    artists = Artist.objects.all()
+
+    return render(request, 'home/index.html', context={"artists": artists})
