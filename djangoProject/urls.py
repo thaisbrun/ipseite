1fe6750a4e2f3ae44af8e934365f2a9fe3c73d9d
Thaïs Brun <travailthais@outlook.fr>
2023-11-24 21:40:02 +0100

    maj 24/11

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -1,28 +1,15 @@
-"""
-URL configuration for djangoProject project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.ipseite, name='ipseite')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='ipseite')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
 from djangoProject import settings
 from ipseite.views import artist_detail
+from accounts.views import signup
 
 urlpatterns = [
                   path('admin/', admin.site.urls),
+                  path('signup/', signup, name="signup"),
                   path('', include('ipseite.urls')),
                   path('artist/<str:slug>', artist_detail, name="artist"),
 
