180f4ab77f5016978abbb95f414c438659e77afd
Thaïs Brun <travailthais@outlook.fr>
2024-02-25 19:30:19 +0100

    avancement du 25/02

--- payments/urls.py
+++ payments/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('create-checkout-session/', views.create_checkout_session),
     path('success/', views.SuccessView.as_view()),  # new
     path('cancelled/', views.CancelledView.as_view()),  # new
+    #path('webhook/', views.stripe_webhook),  # new
 ]
